efe6a0d2f235bbcccaa09b4fac1a2ab1715a9ed7
countzero <finn.rudolph@googlemail.com>
2009-10-16 20:48:51 +0200

    ImageFlow is now object-oriented (call many ImageFlow instances on one page!) Improved startup: now it is called directly after the HTML structure has been loaded Improved code quality: compressable to 8KB, unobtrusive, readable etc. Simplified installation and implementation Added a real loading bar Added options: now everything is optional Added opacity Added move in effect on startup Added image size multiplicator on focus Fixed a bug

--- reflect3.php
+++ reflect3.php
@@ -1,37 +1,25 @@
 <?php
 	/*
-		----------------------------------------------------------------
-		Easy Reflections by Richard Davey, Core PHP (rich@corephp.co.uk)
-		v2 - 2nd March 2007
-        Updates include changes by Monte Ohrt (monte@ohrt.com)
-		----------------------------------------------------------------
+		-------------------------------------------------------------------
+		Easy Reflections v3 by Richard Davey, Core PHP (rich@corephp.co.uk)
+		Released 13th March 2007
+        Includes code submissions from Monte Ohrt (monte@ohrt.com)
+        -------------------------------------------------------------------
 		You are free to use this in any product, or on any web site.
+        I'd appreciate it if you email and tell me where you use it, thanks.
 		Latest builds at: http://reflection.corephp.co.uk
-		----------------------------------------------------------------
+        -------------------------------------------------------------------
 		
 		This script accepts the following $_GET parameters:
 		
-		img		        required	The source image (to reflect)
+		img		        required	The source image to reflect
 		height	        optional	Height of the reflection (% or pixel value)
-		bgc		        optional	Background colour to fade into, default = #000000
         fade_start      optional    Start the alpha fade from whch value? (% value)
         fade_end        optional    End the alpha fade from whch value? (% value)
-		jpeg	        optional	Output will be JPEG at 'param' quality (default 80)
         cache           optional    Save reflection image to the cache? (boolean)
+        tint            optional    Tint the reflection with this colour (hex)
 	*/
-	
-	// Replace special chars to be HTML-Code
-	function stringToHTML($string)
-	{
-		$array_search = array('é', 'è', 'ë', 'ê', 'à', 'ä', 'Ä', 'â', 'ù', 'ü', 'Ü', 'û', 'ö', 'Ö', 'ô', 'ï', 'î');
-		$array_replace = array('&eacute;', '&egrave;',	'&euml;', '&ecirc;', '&agrave;', '&auml;', '&Auml;', '&acirc;', '&ugrave;', '&uuml;', '&Uuml;', '&ucirc;', '&ouml;', '&Ouml;', '&ocirc;', '&iuml;', '&icirc;');
-		$string_return = str_replace($array_search, $array_replace, $string);
-		return $string_return;
-	}
-
-	// CACHE! EVERYTHING! 
-	$_GET['cache'] = 1;
-
+    
 	//	PHP Version sanity check
 	if (version_compare('4.3.2', phpversion()) == 1)
 	{
@@ -46,13 +34,30 @@
 		exit();
 	}
 	
+    //    GD Version check
+    $gd_info = gd_info();
+    
+    if ($gd_info['PNG Support'] == false)
+    {
+        echo 'This version of the GD extension cannot output PNG images.';
+        exit();
+    }
+    
+    if (ereg_replace('[[:alpha:][:space:]()]+', '', $gd_info['GD Version']) < '2.0.1')
+    {
+        echo 'GD library is too old. Version 2.0.1 or later is required, and 2.0.28 is strongly recommended.';
+        exit();
+    }
+
 	//	Our allowed query string parameters
 
     //  To cache or not to cache? that is the question
     if (isset($_GET['cache']))
     {
+
         if ((int) $_GET['cache'] == 1)
         {
+
             $cache = true;
         }
         else
@@ -62,23 +67,20 @@
     }
     else
     {
-        $cache = false;
+        $cache = true;
     }
-
+	
 	//	img (the image to reflect)
 	if (isset($_GET['img']))
 	{
 		$source_image = $_GET['img'];
-
-		//$source_image = utf8_decode($source_image);
-
 		$source_image = str_replace('://','',$source_image);
 		//$source_image = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . $source_image;
         
         if (file_exists($source_image))
         {
             if ($cache)
-            {
+            { 
                 $cache_dir = dirname($source_image);
                 $cache_base = basename($source_image);
                 $cache_file = 'refl_' . md5($_SERVER['REQUEST_URI']) . '_' . $cache_base;
@@ -105,47 +107,51 @@
 		echo 'No source image to reflect supplied';
 		exit();
 	}
-
-	//	bgc (the background colour used, defaults to black if not given)
-	if (isset($_GET['bgc']) == false)
-	{
-		$red = 0;
-		$green = 0;
-		$blue = 0;
-	}
-	else
-	{
-		//	Extract the hex colour
-		$hex_bgc = $_GET['bgc'];
-		
-		//	Does it start with a hash? If so then strip it
-		$hex_bgc = str_replace('#', '', $hex_bgc);
-		
-		switch (strlen($hex_bgc))
-		{
-			case 6:
-				$red = hexdec(substr($hex_bgc, 0, 2));
-				$green = hexdec(substr($hex_bgc, 2, 2));
-				$blue = hexdec(substr($hex_bgc, 4, 2));
-				break;
-				
-			case 3:
-				$red = substr($hex_bgc, 0, 1);
-				$green = substr($hex_bgc, 1, 1);
-				$blue = substr($hex_bgc, 2, 1);
-				$red = hexdec($red . $red);
-				$green = hexdec($green . $green);
-				$blue = hexdec($blue . $blue);
-				break;
-				
-			default:
-				//	Wrong values passed, default to black
-				$red = 0;
-				$green = 0;
-				$blue = 0;
-		}
-	}
 	
+    //    tint (the colour used for the tint, defaults to white if not given)
+    	
+    if (isset($_GET['tint']) == false)
+    {
+        $red = 127;
+        $green = 127;
+        $blue = 127;
+		
+	
+
+    }
+    else
+    {
+        //    Extract the hex colour
+        $hex_bgc = $_GET['tint'];
+        
+        //    Does it start with a hash? If so then strip it
+        $hex_bgc = str_replace('#', '', $hex_bgc);
+        
+        switch (strlen($hex_bgc))
+        {
+            case 6:
+                $red = hexdec(substr($hex_bgc, 0, 2));
+                $green = hexdec(substr($hex_bgc, 2, 2));
+                $blue = hexdec(substr($hex_bgc, 4, 2));
+                break;
+                
+            case 3:
+                $red = substr($hex_bgc, 0, 1);
+                $green = substr($hex_bgc, 1, 1);
+                $blue = substr($hex_bgc, 2, 1);
+                $red = hexdec($red . $red);
+                $green = hexdec($green . $green);
+                $blue = hexdec($blue . $blue);
+                break;
+                
+            default:
+                //    Wrong values passed, default to white
+                $red = 127;
+                $green = 127;
+                $blue = 127;
+        }
+    }
+
 	//	height (how tall should the reflection be?)
 	if (isset($_GET['height']))
 	{
@@ -158,11 +164,15 @@
 			$output_height = (int) substr($output_height, 0, -1);
 
 			//	Gotta love auto type casting ;)
-			if ($output_height < 10)
+			if ($output_height == 100)
 			{
-				$output_height = "0.0$output_height";
+                $output_height = "0.99";
 			}
-			else
+			elseif ($output_height < 10)
+            {
+                $output_height = "0.0$output_height";
+            }
+            else
 			{
 				$output_height = "0.$output_height";
 			}
@@ -254,7 +264,7 @@
 	{
 		//	The output height is a fixed pixel value
 		$new_height = $output_height;
-	}
+	}	
 
 	//	Detect the source image format - only GIF, JPEG and PNG are supported. If you need more, extend this yourself.
 	switch ($type)
@@ -278,17 +288,28 @@
 			echo 'Unsupported image file format.';
 			exit();
 	}
-
-
+	
 	/*
 		----------------------------------------------------------------
 		Build the reflection image
 		----------------------------------------------------------------
 	*/
+	
+
+
 
 	//	We'll store the final reflection in $output. $buffer is for internal use.
 	$output = imagecreatetruecolor($width, $new_height);
 	$buffer = imagecreatetruecolor($width, $new_height);
+    
+    //  Save any alpha data that might have existed in the source image and disable blending
+    imagesavealpha($source, true);
+
+    imagesavealpha($output, true);
+    imagealphablending($output, false);
+
+    imagesavealpha($buffer, true);
+    imagealphablending($buffer, false);
 
 	//	Copy the bottom-most part of the source image into the output
 	imagecopy($output, $source, 0, 0, 0, $height - $new_height, $width, $new_height);
@@ -300,7 +321,7 @@
     }
 
 	$output = $buffer;
-	
+
 	/*
 		----------------------------------------------------------------
 		Apply the fade effect
@@ -309,19 +330,21 @@
 	
 	//	This is quite simple really. There are 127 available levels of alpha, so we just
 	//	step-through the reflected image, drawing a box over the top, with a set alpha level.
-	//	The end result? A cool fade into the background colour given.
+	//	The end result? A cool fade.
 
 	//	There are a maximum of 127 alpha fade steps we can use, so work out the alpha step rate
 
 	$alpha_length = abs($alpha_start - $alpha_end);
 
-	for ($y = 0; $y <= $new_height; $y++)
-	{
-		//  Get % of reflection height
-		$pct = $y / $new_height;
+    imagelayereffect($output, IMG_EFFECT_OVERLAY);
 
-		//  Get % of alpha
-		if ($alpha_start > $alpha_end)
+    for ($y = 0; $y <= $new_height; $y++)
+    {
+        //  Get % of reflection height
+        $pct = $y / $new_height;
+
+        //  Get % of alpha
+        if ($alpha_start > $alpha_end)
         {
             $alpha = (int) ($alpha_start - ($pct * $alpha_length));
         }
@@ -329,19 +352,26 @@
         {
             $alpha = (int) ($alpha_start + ($pct * $alpha_length));
         }
+        
+        //  Rejig it because of the way in which the image effect overlay works
+        $final_alpha = 127 - $alpha;
 
-		imagefilledrectangle($output, 0, $y, $width, $y, imagecolorallocatealpha($output, $red, $green, $blue, $alpha));
-		
-	}
+        //imagefilledrectangle($output, 0, $y, $width, $y, imagecolorallocatealpha($output, 127, 127, 127, $final_alpha));
+        imagefilledrectangle($output, 0, $y, $width, $y, imagecolorallocatealpha($output, $red, $green, $blue, $final_alpha));
+    }
 
-		
-	/*
+    /*
 		----------------------------------------------------------------
 		HACK - Build the reflection image by combining the source 
 		image AND the reflection in one new image!
 		----------------------------------------------------------------
 	*/
 		$finaloutput = imagecreatetruecolor($width, $height+$new_height);
+		imagesavealpha($finaloutput, true);
+
+		$trans_colour = imagecolorallocatealpha($finaloutput, 0, 0, 0, 127);
+		imagefill($finaloutput, 0, 0, $trans_colour);
+
 		imagecopy($finaloutput, $source, 0, 0, 0, 0, $width, $height);
 		imagecopy($finaloutput, $output, 0, $height, 0, 0, $width, $new_height);
 		$output = $finaloutput;
@@ -351,7 +381,6 @@
 		Output our final PNG
 		----------------------------------------------------------------
 	*/
-
 	if (headers_sent())
 	{
 		echo 'Headers already sent, I cannot display an image now. Have you got an extra line-feed in this file somewhere?';
@@ -359,44 +388,15 @@
 	}
 	else
 	{
-		//	If you'd rather output a JPEG instead of a PNG then pass the parameter 'jpeg' (no value needed) on the querystring
+	    //	PNG
+	    header("Content-type: image/png");
+	    imagepng($output);
 
-		if (isset($_GET['png']))
-		{
-			//	PNG
-			header("Content-type: image/png");
-			imagepng($output);
-
-            // Save cached file
-            if ($cache)
-            {
-                imagepng($output, $cache_path);
-            }
-			
-		}
-		else
-		{
-			if(!isset( $_GET['jpeg']))  $_GET['jpeg'] = 90;
-			
-			$quality = (int) $_GET['jpeg'];
-			
-			if ($quality < 1 || $quality > 100)
-			{
-				$quality = 90;
-			}
-			
-			//	JPEG (the final parameter = the quality, 0 = terrible, 100 = pixel perfect)
-			header("Content-type: image/jpeg");
-			imagejpeg($output, '', $quality);
-
-			// Save cached file
-            if ($cache)
-            {
-                imagejpeg($output, $cache_path, $quality);
-            }
-			
-           
-		}
+        // Save cached file
+        if ($cache)
+        {
+            imagepng($output, $cache_path);
+        }
 
 		imagedestroy($output);
 		exit();
