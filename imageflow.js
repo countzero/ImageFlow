bc123f208f1eb34ad3f0efcf0e2446a7b41c22a8
countzero <finn.rudolph@googlemail.com>
2009-10-16 20:53:02 +0200

    Fixed a bug that occured with the option imageFocusM

--- imageflow.js
+++ imageflow.js
@@ -1,6 +1,6 @@
 /*
 Name:       ImageFlow
-Version:    1.0.2 (November 10 2008)
+Version:    1.0.3 (December 26 2008)
 Author:     Finn Rudolph
 Support:    http://finnrudolph.de/ImageFlow
 
@@ -9,8 +9,8 @@ Licence:    ImageFlow is licensed under a Creative Commons
             (http://creativecommons.org/licenses/by-nc/3.0/).
 
             You are free:
-                + to Share — to copy, distribute and transmit the work
-                + to Remix — to adapt the work
+                + to Share - to copy, distribute and transmit the work
+                + to Remix - to adapt the work
 
             Under the following conditions:
                 + Attribution. You must attribute the work in the manner specified by the author or licensor 
@@ -32,7 +32,6 @@ Credits:    This script is based on Michael L. Perrys Cover flow in Javascript [
             [3] http://adomas.org/javascript-mouse-wheel/
             [4] http://tanny.ica.com/ICA/TKO/tkoblog.nsf/dx/domcontentloaded-for-browsers-part-v
 */
-
 
 /* ImageFlow constructor */
 function ImageFlow ()
@@ -91,7 +90,7 @@ function ImageFlow ()
 				this.captionDiv = document.getElementById(thisObject.ImageFlowID+'_caption');
 				this.scrollbarDiv = document.getElementById(thisObject.ImageFlowID+'_scrollbar');
 				this.sliderDiv = document.getElementById(thisObject.ImageFlowID+'_slider');
-			
+
 				this.indexArray = [];
 				this.current = 0;
 				this.imageID = 0;
@@ -123,7 +122,7 @@ function ImageFlow ()
 	this.createStructure = function()
 	{
 		/* Create images div container */
-		var imagesDiv = document.createElement('div');		
+		var imagesDiv = document.createElement('div');
 		imagesDiv.setAttribute('id',thisObject.ImageFlowID+'_images');
 		imagesDiv.setAttribute('class','images');
 		imagesDiv.setAttribute('className','images');
@@ -132,7 +131,7 @@ function ImageFlow ()
 		var node = null;
 		var max = this.ImageFlowDiv.childNodes.length;
 		for(var index = 0; index < max; index++)
-		{ 
+		{
 			node = this.ImageFlowDiv.childNodes[index];
 			if (node && node.nodeType == 1 && node.nodeName == 'IMG')
 			{
@@ -153,19 +152,19 @@ function ImageFlow ()
 				imagesDiv.appendChild(imageNode);
 			}
 		}
-					
+
 		/* Create loading text container */
 		var loadingP = document.createElement('p');
 		var loadingText = document.createTextNode(' ');
 		loadingP.setAttribute('id',thisObject.ImageFlowID+'_loading_txt');
 		loadingP.appendChild(loadingText);
-		
+
 		/* Create loading div container */
 		var loadingDiv = document.createElement('div');
 		loadingDiv.setAttribute('id',thisObject.ImageFlowID+'_loading');
 		loadingDiv.setAttribute('class','loading');
 		loadingDiv.setAttribute('className','loading');
-			
+
 		/* Create loading bar div container inside the loading div */
 		var loadingBarDiv = document.createElement('div');
 		loadingBarDiv.setAttribute('id',thisObject.ImageFlowID+'_loading_bar');
@@ -189,23 +188,23 @@ function ImageFlow ()
 		sliderDiv.setAttribute('class','slider');
 		sliderDiv.setAttribute('className','slider');
 		scrollbarDiv.appendChild(sliderDiv);
-		
+
 		/* Update document structure and return true on success */
 		var success = false;
-		if (thisObject.ImageFlowDiv.appendChild(imagesDiv) && 
-			thisObject.ImageFlowDiv.appendChild(loadingP) && 
-			thisObject.ImageFlowDiv.appendChild(loadingDiv) && 
-			thisObject.ImageFlowDiv.appendChild(captionDiv) && 
+		if (thisObject.ImageFlowDiv.appendChild(imagesDiv) &&
+			thisObject.ImageFlowDiv.appendChild(loadingP) &&
+			thisObject.ImageFlowDiv.appendChild(loadingDiv) &&
+			thisObject.ImageFlowDiv.appendChild(captionDiv) &&
 			thisObject.ImageFlowDiv.appendChild(scrollbarDiv))
 		{
 			/* Remove image nodes outside the images div */
 			for(index = 0; index < max; index++)
-			{ 
+			{
 				node = this.ImageFlowDiv.childNodes[index];
 				if (node && node.nodeType == 1 && node.nodeName == 'IMG')
 				{
 					this.ImageFlowDiv.removeChild(node);
-				}				
+				}
 			}
 			success = true;
 		}
@@ -238,7 +237,7 @@ function ImageFlow ()
 
 			/* Refresh ImageFlow on window resize - delay adding this event for the IE */
 			window.setTimeout(thisObject.addResizeEvent, 1000);
-						
+
 			/* Fixes the back button issue */
 			window.onunload = function()
 			{
@@ -276,7 +275,7 @@ function ImageFlow ()
 		}
 	};
 
-	
+
 	/* Returns loaded images in percent, sets loading bar width and loading text */
 	this.loadingStatus = function()
 	{
@@ -316,10 +315,10 @@ function ImageFlow ()
 		this.size = thisObject.iWidth * 0.5;
 		this.scrollbarWidth = Math.round(thisObject.iWidth * 0.6);
 		this.sliderWidth = thisObject.sliderWidth * 0.5;
-		
+
 		/* Change imageflow div properties */
 		thisObject.ImageFlowDiv.style.height = thisObject.maxHeight + 'px';
-		
+
 		/* Change images div properties */
 		thisObject.imagesDiv.style.height = Math.round(thisObject.iWidth * 0.338) + 'px';
 
@@ -331,30 +330,30 @@ function ImageFlow ()
 		thisObject.scrollbarDiv.style.width = thisObject.scrollbarWidth + 'px';
 		thisObject.scrollbarDiv.style.marginTop = Math.round(thisObject.iWidth * 0.02) + 'px';
 		thisObject.scrollbarDiv.style.marginLeft = Math.round(thisObject.iWidth * 0.2) + 'px';
-		
+
 		/* Set slider attributes */
 		thisObject.sliderDiv.style.cursor = thisObject.sliderCursor;
 		thisObject.sliderDiv.onmousedown = function () { thisObject.MouseDrag.start(this); return false;};
-		
+
 		/* Set the reflection multiplicator */
 		var multi = (thisObject.reflections === true) ? thisObject.reflectionP + 1 : 1;
-		
+
 		/* Set image attributes */
 		var max = thisObject.imagesDiv.childNodes.length;
 		var i = 0;
 		var image = null;
 		for (var index = 0; index < max; index++)
-		{ 
+		{
 			image = thisObject.imagesDiv.childNodes[index];
 			if(image !== null && image.nodeType == 1 && image.nodeName == 'IMG')
 			{
 				this.indexArray[i] = index;
-				
+
 				/* Set image attributes to store values */
 				image.url = image.getAttribute('longdesc');
 				image.xPosition = (-i * thisObject.xStep);
 				image.i = i;
-				
+
 				/* Add width and height as attributes only once */
 				if(thisObject.firstRefresh)
 				{
@@ -369,7 +368,7 @@ function ImageFlow ()
 					}
 				}
 				/* Check source image format. Get image height minus reflection height! */
-				if((image.w) > (image.h / (thisObject.reflectionP + 1))) 
+				if((image.w) > (image.h / (thisObject.reflectionP + 1)))
 				{
 					/* Landscape format */
 					image.pc = 118;
@@ -388,7 +387,7 @@ function ImageFlow ()
 			}
 		}
 		this.max = thisObject.indexArray.length;
-		
+
 		/* Reset variable */
 		if(thisObject.firstRefresh)
 		{
@@ -399,7 +398,7 @@ function ImageFlow ()
 		thisObject.moveTo(thisObject.current);
 		thisObject.glideTo(thisObject.imageID);
 	};
-	
+
 
 	/* Main animation function */
 	this.moveTo = function(x)
@@ -419,14 +418,14 @@ function ImageFlow ()
 				image.style.visibility = 'hidden';
 				image.style.display = 'none';
 			}
-			else 
+			else
 			{
 				var z = Math.sqrt(10000 + x * x) + 100;
 				var xs = x / z * thisObject.size + thisObject.size;
 
 				/* Still hide images until they are processed, but set display style to block */
 				image.style.display = 'block';
-			
+
 				/* Process new image height and image width */
 				var newImageH = (image.h / image.w * image.pc) / z * thisObject.size;
 				var newImageW = 0;
@@ -446,9 +445,9 @@ function ImageFlow ()
 				/* Set new image properties */
 				image.style.left = xs - (image.pc / 2) / z * thisObject.size + 'px';
 				if(newImageW && newImageH)
-				{ 
-					image.style.height = newImageH + 'px'; 
-					image.style.width = newImageW + 'px'; 
+				{
+					image.style.height = newImageH + 'px';
+					image.style.width = newImageW + 'px';
 					image.style.top = newImageTop + 'px';
 				}
 				image.style.visibility = 'visible';
@@ -464,7 +463,7 @@ function ImageFlow ()
 						this.zIndex = thisObject.zIndex - 1;
 						break;
 				}
-				
+
 				/* Change zIndex and onclick function of the focussed image */
 				switch ( image.i == thisObject.imageID )
 				{
@@ -489,7 +488,7 @@ function ImageFlow ()
 
 	/* Initializes image gliding animation */
 	this.glideTo = function(imageID)
-	{	
+	{
 		/* Calculate new image position target */
 		var x = -imageID * thisObject.xStep;
 		this.target = x;
@@ -500,7 +499,7 @@ function ImageFlow ()
 		var caption = thisObject.imagesDiv.childNodes[imageID].getAttribute('alt');
 		if (caption === '' || thisObject.captions === false)
 		{
-			caption = '&nbsp;'; 
+			caption = '&nbsp;';
 		}
 		thisObject.captionDiv.innerHTML = caption;
 
@@ -510,14 +509,14 @@ function ImageFlow ()
 			this.newSliderX = (imageID * thisObject.scrollbarWidth) / (thisObject.max-1) - thisObject.MouseDrag.newX;
 			thisObject.sliderDiv.style.marginLeft = (thisObject.newSliderX - thisObject.sliderWidth) + 'px';
 		}
-		
+
 		/* Only process if opacity or a multiplicator for the focussed image has been set */
 		if(thisObject.opacity === true || thisObject.imageFocusM !== thisObject.defaults.imageFocusM)
 		{
 			/* Set opacity for centered image */
 			thisObject.setOpacity(thisObject.imagesDiv.childNodes[imageID], thisObject.opacityArray[0]);
-			thisObject.imagesDiv.childNodes[imageID].pc = thisObject.imagesDiv.childNodes[imageID].pc * thisObject.imageFocusM;
-			
+            thisObject.imagesDiv.childNodes[imageID].pc = thisObject.imagesDiv.childNodes[imageID].pc * thisObject.imageFocusM;
+ 
 			/* Set opacity for the other images that are displayed */
 			var opacityValue = 0;
 			var rightID = 0;
@@ -536,7 +535,7 @@ function ImageFlow ()
 				}
 
 				rightID = imageID + i;
-				leftID = imageID - i;				
+				leftID = imageID - i;
 
 				if (rightID < thisObject.max)
 				{
@@ -551,7 +550,7 @@ function ImageFlow ()
 			}
 		}
 
-		/* animate gliding to new x position */
+		/* Animate gliding to new x position */
 		if (thisObject.busy === false)
 		{
 			window.setTimeout(thisObject.animate, 50);
@@ -559,11 +558,11 @@ function ImageFlow ()
 		}
 	};
 
-	
-	/* animates image gliding */
+
+	/* Animates image gliding */
 	this.animate = function()
 	{
-		switch (thisObject.target < thisObject.current-1 || thisObject.target > thisObject.current+1) 
+		switch (thisObject.target < thisObject.current-1 || thisObject.target > thisObject.current+1)
 		{
 			case true:
 				thisObject.moveTo(thisObject.current + (thisObject.target-thisObject.current)/3);
@@ -588,7 +587,7 @@ function ImageFlow ()
 		}
 	};
 
-	
+
 	/* Initialize mouse wheel support */
 	this.initMouseWheel = function()
 	{
@@ -657,7 +656,7 @@ function ImageFlow ()
 		}
 	};
 
-	
+
 	/* MouseDrag */
 	this.MouseDrag =
 	{
@@ -666,16 +665,16 @@ function ImageFlow ()
 		mouseX: 0,
 		newX: 0,
 		busy: false,
-		
+
 		/* Init mouse event listener */
-		init: function() 
+		init: function()
 		{
 			thisObject.addEvent(thisObject.ImageFlowDiv,'mousemove',thisObject.MouseDrag.drag);
 			thisObject.addEvent(thisObject.ImageFlowDiv,'mouseup',thisObject.MouseDrag.stop);
 			thisObject.addEvent(document,'mouseup',thisObject.MouseDrag.stop);
-	
+
 			/* Avoid text and image selection while dragging  */
-			thisObject.ImageFlowDiv.onselectstart = function () 
+			thisObject.ImageFlowDiv.onselectstart = function ()
 			{
 				var selection = true;
 				if (thisObject.MouseDrag.busy === true)
@@ -686,11 +685,11 @@ function ImageFlow ()
 			};
 		},
 
-		start: function(o) 
+		start: function(o)
 		{
 			thisObject.MouseDrag.object = o;
 			thisObject.MouseDrag.objectX = thisObject.MouseDrag.mouseX - o.offsetLeft + thisObject.newSliderX;
-			
+
 		},
 
 		stop: function()
@@ -699,11 +698,11 @@ function ImageFlow ()
 			thisObject.MouseDrag.busy = false;
 		},
 
-		drag: function(e) 
+		drag: function(e)
 		{
 			var posx = 0;
 			if (!e)
-			{ 
+			{
 				e = window.event;
 			}
 			if (e.pageX)
@@ -729,24 +728,26 @@ function ImageFlow ()
 				{
 					newX = thisObject.scrollbarWidth - thisObject.newSliderX;
 				}
-				
+
 				/* Set new slider position */
 				var step = (newX + thisObject.newSliderX) / (thisObject.scrollbarWidth / (thisObject.max-1));
 				var imageID = Math.round(step);
 				thisObject.MouseDrag.newX = newX;
 				thisObject.MouseDrag.object.style.left = newX + 'px';
-				thisObject.glideTo(imageID);
-
+                if(thisObject.imageID !== imageID)
+                {
+                    thisObject.glideTo(imageID);
+                }
 				thisObject.MouseDrag.busy = true;
 			}
 		}
 	};
-	
+
 	/* Key support */
 	this.Key =
 	{
 		/* Init key event listener */
-		init: function() 
+		init: function()
 		{
 			document.onkeydown = function(event){ thisObject.Key.handle(event); };
 		},
@@ -761,14 +762,14 @@ function ImageFlow ()
 				case 39:
 					thisObject.handleMouseWheel(-1);
 					break;
-				
+
 				/* Left arrow key */
 				case 37:
 					thisObject.handleMouseWheel(1);
 					break;
 			}
 		},
-		
+
 		/* Get the current keycode */
 		get: function(event)
 		{
@@ -791,7 +792,7 @@ function ImageFlow ()
 			obj[type+fn] = function() { obj["e"+type+fn]( window.event ); };
 			obj.attachEvent( "on"+type, obj[type+fn] );
 		}
-	};	
+	};
 
 	/* Adds functions to the window.onresize event - can not be done by addEvent */
 	this.addResizeEvent = function()
@@ -882,7 +883,7 @@ var domReadyEvent =
 		{
 			return;
 		}
-	
+
 		/* First, check for Safari or KHTML.*/
 		if(/KHTML|WebKit/i.test(navigator.userAgent))
 		{
@@ -905,8 +906,8 @@ var domReadyEvent =
 		/* Check for custom developer provided function.*/
 		if(typeof this.DOMContentLoadedCustom === "function")
 		{
-			/* if DOM methods are supported, and the body element exists (using a double-check 
-			including document.body, for the benefit of older moz builds [eg ns7.1] in which 
+			/* if DOM methods are supported, and the body element exists (using a double-check
+			including document.body, for the benefit of older moz builds [eg ns7.1] in which
 			getElementsByTagName('body')[0] is undefined, unless this script is in the body section) */
 			if(typeof document.getElementsByTagName !== 'undefined' && (document.getElementsByTagName('body')[0] !== null || document.body !== null))
 			{
@@ -940,7 +941,7 @@ var domReadyEvent =
 		{
 			domReadyEvent.run();
 		}
-		
+
 		/* Just in case window.onload happens first, add it to onload using an available method.*/
 		if(typeof addEvent !== "undefined")
 		{
@@ -963,7 +964,7 @@ var domReadyEvent =
 		{
 			window.onload = run;
 		}
-		
+
 		/* for Internet Explorer */
 		/*@cc_on
 			@if (@_win32 || @_win64)
