20af45c25658d32f19e1f3df469615f93908e2c1
countzero <finn.rudolph@googlemail.com>
2009-10-16 20:45:41 +0200

    Added different cursor types Added configuration variables Changed implementation: all JavaScript is now stored inside the imageflow.js Changed the captions handling: now the caption text is directly stored in the alt attribute of the image tag Fixed a bug

--- imageflow.js
+++ imageflow.js
@@ -1,5 +1,5 @@
 /**
- *	ImageFlow 0.6
+ *	ImageFlow 0.7
  *
  *	This code is based on Michael L. Perrys Cover flow in Javascript.
  *	For he wrote that "You can take this code and use it as your own" [1]
@@ -20,29 +20,37 @@
  */
 
 
+/* Configuration variables */
+var conf_reflection_p = 0.5;		// Sets the height of the reflection in % of the source image 
+var conf_focus = 4;					// Sets the numbers of images on each side of the focussed one
+var conf_slider_width = 14;			// Sets the px width of the slider div
+var conf_images_cursor = "pointer";	// Sets the cursor type for all images default is "default"
+var conf_slider_cursor = "default";	// Sets the slider cursor type: try "e-resize" default is "default"
+
+/* Id names used in the HTML */
+var conf_imageflow = "imageflow";	// Default is "imageflow"
+var conf_loading = "loading";		// Default is "loading"
+var conf_images = "images";			// Default is "images"
+var conf_captions = "captions";		// Default is "captions"
+var conf_scrollbar = "scrollbar";	// Default is "scrollbar"
+var conf_slider = "slider"			// Default is "slider"
+
 /* Define global variables */
-var caption_id = "i1";
-var new_caption_id = "";
+var caption_id = 0;
+var new_caption_id = 0;
 var current = 0;
 var target = 0;
 var mem_target = 0;
 var timer = 0;
 var array_images = new Array();
 var new_slider_pos = 0;
-var dragging=false;
+var dragging = false;
 var dragobject = null;
 var dragx = 0;
 var posx = 0;
 var new_posx = 0;
+var xstep = 150;
 
-/* This variable must be changed to the used reflection image height in % of the source image */
-var reflection_p = 0.5;
-
-/* This variable sets the focus: max number of images displayed on each side of the current one */
-var focus = 4;
-
-/* This variable must be set to the width of the slider div in px */
-var slider_width = 14;
 
 function step()
 {
@@ -70,17 +78,18 @@ function glideTo(x, new_caption_id)
 		window.setTimeout(step, 50);
 		timer = 1;
 	}
-
+	
 	/* Display new caption */
 	caption_id = new_caption_id;
-	caption = document.getElementById(caption_id)
-	if (caption) caption_div.innerHTML = caption.innerHTML;
+	caption = img_div.childNodes.item(array_images[caption_id]).getAttribute('alt');
+	if (caption == "") caption = "&nbsp;";
+	caption_div.innerHTML = caption;
 
 	/* Set scrollbar slider to new position */
 	if (dragging == false)
 	{
-		new_slider_pos = (scrollbar_width * (-(x*100/((max-1)*150))) / 100) - new_posx;
-		slider_div.style.marginLeft = (new_slider_pos - slider_width) + "px";
+		new_slider_pos = (scrollbar_width * (-(x*100/((max-1)*xstep))) / 100) - new_posx;
+		slider_div.style.marginLeft = (new_slider_pos - conf_slider_width) + "px";
 	}
 }
 
@@ -93,13 +102,13 @@ function moveTo(x)
 	for (var index = 0; index < max; index++)
 	{ 
 		var image = img_div.childNodes.item(array_images[index]);
-		var current_image = index * -150;
+		var current_image = index * -xstep;
 
-		/* Don't display images that are not focussed */
-		if ((current_image+max_focus) < mem_target || (current_image-max_focus) > mem_target)
+		/* Don't display images that are not conf_focussed */
+		if ((current_image+max_conf_focus) < mem_target || (current_image-max_conf_focus) > mem_target)
 		{
-			image.style.visibility="hidden";
-			image.style.display="none";
+			image.style.visibility = "hidden";
+			image.style.display = "none";
 		}
 		else 
 		{
@@ -107,14 +116,14 @@ function moveTo(x)
 			var xs = x / z * size + size;
 
 			/* Still hide images until they are processed, but set display style to block */
-			image.style.display="block";
+			image.style.display = "block";
 	
 			/* Get current image properties */
 			var img_h = image.height;
 			var img_w = image.width;
 			
 			/* Check source image format. Get image height minus reflection height! */
-			switch ((img_w + 1) > (img_h / (reflection_p + 1))) 
+			switch ((img_w + 1) > (img_h / (conf_reflection_p + 1))) 
 			{
 				/* Landscape format */
 				case true:
@@ -129,14 +138,14 @@ function moveTo(x)
 			
 			/* Process new image height and top spacing */
 			var new_img_h = (img_h / img_w * img_percent) / z * size;
-			var new_img_top = (images_width * 0.34 - new_img_h) + images_top + ((new_img_h / (reflection_p + 1)) * reflection_p);
+			var new_img_top = (images_width * 0.34 - new_img_h) + images_top + ((new_img_h / (conf_reflection_p + 1)) * conf_reflection_p);
 		
 			/* Set new image properties */
 			image.style.left = xs - (img_percent / 2) / z * size + images_left + "px";
 			image.style.height = new_img_h + "px";
-			image.style.width= "";
+			image.style.width = "";
 			image.style.top = new_img_top + "px";
-		 	image.style.visibility="visible";
+		 	image.style.visibility = "visible";
 
 			/* Set image layer through zIndex */
 			switch ( x < 0) 
@@ -149,33 +158,35 @@ function moveTo(x)
 					zIndex = zIndex -1;
 					break;
 			}
+
+			/* Make sure, that the conf_focussed image is always on top */
+			if((current_image-100 < mem_target) && (current_image+100 > mem_target)){
+				zIndex = zIndex +1;
+			}
 			image.style.zIndex = zIndex;
 		}
-		x += 150;
+		x += xstep;
 	}
 }
 
 /* Main function */
 function refresh()
 {
-
-	/* Cache document objects in global script variables */
-	imageflow_div = document.getElementById("imageflow");
-	img_div = document.getElementById("images");
-	scrollbar_div = document.getElementById("scrollbar");
-	slider_div = document.getElementById("scrollbar_slider");
-	caption_div = document.getElementById("captions");
-	caption_div.innerHTML = document.getElementById(caption_id).innerHTML;
+	/* Cache document objects in global variables */
+	imageflow_div = document.getElementById(conf_imageflow);
+	img_div = document.getElementById(conf_images);
+	scrollbar_div = document.getElementById(conf_scrollbar);
+	slider_div = document.getElementById(conf_slider);
+	caption_div = document.getElementById(conf_captions);
 
 	/* Cache global variables, that only change on refresh */
 	images_width = img_div.offsetWidth;
 	images_top = imageflow_div.offsetTop;
 	images_left = imageflow_div.offsetLeft;
-	max_focus = focus * 150;
+	max_conf_focus = conf_focus * xstep;
 	size = images_width * 0.5;
-	max = img_div.childNodes.length;
 	scrollbar_width = images_width * 0.6;
-	slider_width = slider_width * 0.5;
+	conf_slider_width = conf_slider_width * 0.5;
 	
 	/* Change imageflow div properties */
 	imageflow_div.style.height = images_width * 0.51 + "px";
@@ -192,15 +203,29 @@ function refresh()
 	scrollbar_div.style.marginLeft = images_width * 0.2 + "px";
 	scrollbar_div.style.width = scrollbar_width + "px";
 	
+	/* Set slider attributes */
+	slider_div.onmousedown = function () { dragstart(this); };
+	slider_div.style.cursor = conf_slider_cursor;
+
 	/* Cache correct node type indices in an array */
-	var count=0;
+	max = img_div.childNodes.length;
+	var i = 0;
 	for (var index = 0; index < max; index++)
 	{ 
 		var image = img_div.childNodes.item(index);
 		if (image.nodeType == 1)
 		{
-			array_images[count] = index;
-			count++;
+			array_images[i] = index;
+			
+			/* Set correct image onclick by adding i and x_pos as attributes! */
+			image.onclick = function() { glideTo(this.x_pos, this.i); }
+			image.x_pos = (-i * xstep);
+			image.i = i;
+			
+			/* Set image cursor type */
+			image.style.cursor = conf_images_cursor;
+
+			i++;
 		}
 	}
 	max = array_images.length;
@@ -223,10 +248,10 @@ function hide(id) {
 
 /* Hide loading bar, show content and initialize mouse event listening after loading */
 window.onload = function() {
-	hide('loading');
+	hide(conf_loading);
 	refresh();
-	show('images');
-	show('scrollbar');
+	show(conf_images);
+	show(conf_scrollbar);
 	initMouseWheel();
 	initMouseDrag();
 }
@@ -237,39 +262,33 @@ window.onresize = refresh;
 /* Handle the wheel angle change (delta) of the mouse wheel */
 function handle(delta) 
 {
-	var change=false;
-
-	/* Extract the integer from the caption_id */
-	var caption_id_int = caption_id.substr(1);
-	caption_id_int = parseInt(caption_id_int);
-
+	var change = false;
 	switch (delta > 0) 
 	{
 		case true:
-			if(caption_id_int != 1 && target <= 0)
+			if(caption_id >= 1)
 			{
-				target = target + 150;
-				new_caption_id = 'i' + (caption_id_int - 1);
-				change=true;
+				target = target + xstep;
+				new_caption_id = caption_id - 1;
+				change = true;
 			}
 			break;
 
 		default:
-			if(caption_id_int < max)
+			if(caption_id < (max-1))
 			{
-				target = target - 150;
-				new_caption_id = 'i' + (caption_id_int + 1);
-				change=true;
+				target = target - xstep;
+				new_caption_id = caption_id + 1;
+				change = true;
 			}
 			break;
 	}
 
 	/* Glide to next (mouse wheel down) / previous (mouse wheel up) image */
-	if (change==true)
+	if (change == true)
 	{
 		glideTo(target, new_caption_id);
 	}
-	
 }
 
 /* Event handler for mouse wheel event */
@@ -318,7 +337,7 @@ function drag(e)
 	if(dragobject != null) 
 	{
 		dragging = true;
-		new_posx = (posx - dragx) + slider_width;
+		new_posx = (posx - dragx) + conf_slider_width;
 
 		/* Make sure, that the slider is moved in proper relation to previous movements by the glideTo function */
 		if(new_posx < ( - new_slider_pos)) new_posx = - new_slider_pos;
@@ -327,8 +346,8 @@ function drag(e)
 		var slider_pos = (new_posx + new_slider_pos);
 		var step_width = slider_pos / ((scrollbar_width) / (max-1));
 		var image_number = Math.round(step_width);
-		var new_target = (image_number) * -150;
-		var new_caption_id = 'i' + (image_number+1);
+		var new_target = (image_number) * -xstep;
+		var new_caption_id = image_number;
 
 		dragobject.style.left = new_posx + "px";
 		glideTo(new_target, new_caption_id);
@@ -341,7 +360,6 @@ function initMouseDrag()
 	document.onmousemove = drag;
 	document.onmouseup = dragstop;
 }
-
 
 function getKeyCode(event) {
    event = event || window.event;
